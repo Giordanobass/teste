6920a6fff7fd826508d045ee680f543a80d70c65
Giordanobass <giordanoamaral@gmail.com>
2019-12-12 14:39:50 -0300

    Acrescentada mensagem bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,23 +11,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		Employees empregado = new Employees();
-		
-		System.out.print("Enter the employee id that will salary increase: ");
-		empregado.setId(sc.nextInt());
-		System.out.println(" ");
-		System.out.print("Enter the name: ");
-		empregado.setName(sc.nextLine());
-		System.out.print("Enter with salary: ");
-		empregado.setSalary(sc.nextDouble());
-		
-		System.out.println("Enter the porcentage: ");
-		empregado.increaseSalary(sc.nextDouble());
-		
-		System.out.println("Salario: " + empregado.getSalary());
-		
-		sc.close();
-	}
+		System.out.println("boa tarde");
 
+	}
 }
